4a5ea10ec337a484340a51594472b947f6cb6734
Minuddin Ahmed Rana <minuddinrana@gmail.com>
2018-01-18 12:13:27 +0600

    Persistence and fair dispatch

--- work-on-queue/new_task.py
+++ work-on-queue/new_task.py
@@ -1,20 +1,25 @@
 import sys
 import pika
+import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='task_queue')
+#channel.queue_declare(queue='task_queue')
+"""create durable queue"""
+channel.queue_declare(queue='task_queue', durable=True)
 
 message = ''.join(sys.argv[1:]) or 'Hello World'
 
 for i in range(100):
+    print('Generating Task# ', i)
     channel.basic_publish(
-	    exchange='',
+        exchange='',
 	    routing_key='task_queue',
 	    body="#Task "+str(i),
 	    properties=pika.BasicProperties(
-		delivery_mode=2
-	    )
+	       delivery_mode=2
+        )
     )
 
+connection.close()
 #message = ''.join(sys.argv[1:]) or 'hello world'
--- work-on-queue/worker.py
+++ work-on-queue/worker.py
@@ -1,17 +1,26 @@
-import time
 import pika
+import random
 
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-channel.queue_declare('task_queue')
+channel.queue_declare('task_queue', durable=True)
 
 
 def callback(ch, method, properties, body):
-    print('The body', body)
-    time.sleep(5)
-    print(body, ' task done')
+    print('Starting Task#', body)
+    l = [random.random() for i in range(2000000)]
+    l.sort()
+    print('Task#', body, ' Done')
+    # we are sending manual acknowledgement that the task is done
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
-channel.basic_consume(callback, queue='task_queue', no_ack=True)
+"""by using no_ack we are telling that not manual acknowledgement needed"""
+#channel.basic_consume(callback, queue='task_queue', no_ack=True)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback, queue='task_queue')
 channel.start_consuming()
+
+connection.close()
